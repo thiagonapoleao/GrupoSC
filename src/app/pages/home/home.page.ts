fda52253fdcea3f0dba947bc1a2de316e53ab87a
thiagonapoleao <thc.napoleao@gmail.com>
2019-08-19 22:31:52 -0300

    ok

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { IonSlides } from '@ionic/angular';
-
+import { DadosSCService } from '../services/dados-sc.service';
+import { Upm } from '../upm/upm.model';
 
 @Component({
   selector: 'app-home',
@@ -10,11 +11,30 @@ import { IonSlides } from '@ionic/angular';
 export class HomePage implements OnInit {
   @ViewChild(IonSlides,{static:false}) slides: IonSlides;
 
-  constructor() {
- 
+  upms: Upm[];
+  upmTotal : Upm = new Upm();
+
+
+
+  constructor(public service: DadosSCService) {
   }
  
   ngOnInit() {
+    this.getDados();
+    console.log 
   }
-
+  getDados() {
+    this.service.getUpm().then((result: any[]) => {
+      this.upms = result['upms'];
+    }).catch((error: any) => {
+      console.error("error: " + error);
+    }).finally(() =>{
+      this.upms.forEach(u => {
+        if(u.indicador == "Total"){
+           this.upmTotal = u;
+        }
+      });
+    });
+    
+  }
 }
